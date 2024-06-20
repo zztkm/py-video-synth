58b4d4a99821d6223885411ec0eab581bcb496e9
zztkm <zztkm@tsurutatakumi.info>
2024-06-21 00:43:27 +0900

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# py-video-synth
+
+Describe your project here.
+
+```bash
+brew install portaudio
+```
+
+
+## memo
+
+- audio.onset の代替
+    - https://librosa.org/doc/latest/onset.html#module-librosa.onset
---
+++ src/main.py
@@ -0,0 +1,27 @@
+import pyaudio
+import sys
+import numpy as np
+#import aubio
+
+import pygame
+import random
+
+from threading import Thread
+
+import queue
+import time
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-input", required=False, type=int, help="Audio Input Device")
+parser.add_argument("-f", action="store_true", help="Run in Fullscreen Mode")
+args = parser.parse_args()
+
+if not args.input:
+    print("No input device specified. Printing list of input devices now: ")
+    p = pyaudio.PyAudio()
+    for i in range(p.get_device_count()):
+        print("Device number (%i): %s" % (i, p.get_device_info_by_index(i).get('name')))
+    print("Run this program with -input 1, or the number of the input you'd like to use.")
+    exit()
